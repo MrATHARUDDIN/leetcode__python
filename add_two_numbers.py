91746a1d6e603feda3f4ac97f7bb4aabe8c550e7
MrATHARUDDIN <atharuddin.bd6@gmail.com>
2025-07-04 16:52:27 +0200

    question added

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,3 +1,13 @@
+# You are given two non-empty linked lists representing two non-negative integers.
+# The digits are stored in reverse order, and each of their nodes contains a single digit.
+# Add the two numbers and return the sum as a linked list (or list) in reverse order.
+
+# Example:-
+# l1 = [3, 4, 2]  # Represents 243
+# l2 = [1, 2, 3]  # Represents 321
+
+# 243 + 321 = 564
+
 def addTwoNumbers(l1, l2):
     num1 = int("".join(str(digit) for digit in reversed(l1)))  
 
